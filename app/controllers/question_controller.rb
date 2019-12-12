e0719f9c599b508b7601e6d47f079f2341df7bd3
IkegzI <kegz@oit-02-nettop.localdomain>
2019-12-12 12:17:56 +0800

    form for answer

--- app/controllers/question_controller.rb
+++ app/controllers/question_controller.rb
@@ -7,11 +7,20 @@ class QuestionController < ApplicationController
   end
 
   def create
+    quest = Question.new(params_question)
+    quest.save
+    redirect_to question_index_path
   end
 
   def edit
   end
 
   def destroy
+  end
+
+  private
+
+  def params_question
+    params.require(:question).permit(:header, :text)
   end
 end
